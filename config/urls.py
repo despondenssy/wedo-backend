59be284fedfadf826bb07cc57787b0d1a792d68e
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2026-04-12 02:18:03 +0300

    feat(docs): add OpenAPI documentation setup

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='api-schema'), name='redoc'),
     path('', include('users.urls')),
     path('', include('activities.urls')),
     path('', include('participation.urls')),
