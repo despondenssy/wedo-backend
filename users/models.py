111310e4a419b1f03d1958755b0197ca57bafa5c
PolinaKliushova <prekrasnayapolyachka@gmail.com>
2026-04-11 21:42:20 +0300

    fix(users): handle empty qr token expiration check

--- users/models.py
+++ users/models.py
@@ -117,4 +117,6 @@ class QrToken(models.Model):
     @property
     def is_expired(self):
         from django.utils import timezone
+        if self.expires_at is None: #если expires_at пустое, выкидывал ошибку
+            return False
         return timezone.now() > self.expires_at
