bad1c5ef29caa9d62118ebf75196140a431cfdab
lisette <m.elizaveta04@gmail.com>
2026-04-16 23:49:21 +0300

    feat(subscriptions): add POST /subscriptions alias

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -3,5 +3,6 @@ from .views import SubscriptionsListView, SubscriptionDetailView
 
 urlpatterns = [
     path('me/subscriptions', SubscriptionsListView.as_view()),
+    path('subscriptions', SubscriptionsListView.as_view()),
     path('subscriptions/<int:user_id>', SubscriptionDetailView.as_view()),
 ]
